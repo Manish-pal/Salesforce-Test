2aaa979affac28a8c3a91cc0be76c51925763a76
Manish Kumar <kumarmanish2048@gmail.com>
2021-04-05 00:37:12 +0530

    Added Checkbox functionality
    Checkbox functionality for showing the message once checked

--- Lightning Components/Registration_ComponentController.js
+++ Lightning Components/Registration_ComponentController.js
@@ -3,26 +3,39 @@
 // parameter2 -> refers the event action
 // parameter -> always refer to helper resource
 
-({
+/*({
 	myAction : function(component, event, helper) {
 		
 	}
     
-})
+})*/
 
 ({    doSubmit : function(component, event, helper) {
-    	// accessing the value in attribute abc
-    	var initialABCValue = component.get("v.anc");
-            if(initialABCValue == "true") {
-                alert("Value is true");
-                // setting the value to be false
-                component.set("v.abc", "false");
-            }else {
-                
-                alert("Value is false");
-                //setting the value to be true
-                component.set("v.abc","true")
-            }
-	}
- 
- })
+    // accessing the value in attribute abc
+    var initialABCValue = component.get("v.abc");
+        if(initialABCValue == "true") {
+            alert("Value is true");
+            // setting the value to be false
+            component.set("v.abc", "false");
+        }
+        else {
+            alert("Value is Falseee");
+            //setting the value to be true
+            component.set("v.abc","true");
+        }
+},
+
+//Calling a helper function from a controller
+OnClickCheckBox : function(component, event, helper) {
+
+    //Calling function defined in helper
+    //helper.<functionNameinHELPER>
+    console.log("From controller");
+    helper.onClickCheckBoxhelper(component,event);
+  
+}
+  
+
+})
+
+
